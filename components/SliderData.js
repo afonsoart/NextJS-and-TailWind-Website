4d564c18061d22805e89af702bebfddfc210bf1c
Afonso Ramon de Araujo Teles <afonso1907@hotmail.com>
2022-12-11 16:21:32 -0300

    Slider Component - Lazy Loading Images

---
+++ components/SliderData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+    {
+        image:
+            'https://images.unsplash.com/photo-1618861615301-31ec0bba7dce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    },
+    {
+        image:
+            'https://images.unsplash.com/photo-1621264404819-502db472acfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    },
+    {
+        image:
+            'https://images.unsplash.com/photo-1516283182395-4b90237bff2e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80'
+    },
+    {
+        image:
+            'https://images.unsplash.com/photo-1534455700361-eca9c3dbc981?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    },
+    {
+        image:
+            'https://images.unsplash.com/photo-1543427099-ca22f2550846?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    },
+];
